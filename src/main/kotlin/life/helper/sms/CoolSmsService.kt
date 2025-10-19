c73cb7df65cc91d12edd3d22b9701a0d9ae9311c
cousim46 <wjdghldns24@naver.com>
2025-10-19 17:29:19 +0900

    feat : 문자 발송 구현체 구현 (coolSms)

---
+++ src/main/kotlin/life/helper/sms/CoolSmsService.kt
@@ -0,0 +1,25 @@
+package life.helper.sms
+
+import com.solapi.sdk.SolapiClient
+import com.solapi.sdk.message.model.Message
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class CoolSmsService(
+  @Value("\${sms.key}")
+  private val smsKey: String,
+  @Value("\${sms.secret_key}")
+  private val smsSecretKey: String,
+  @Value("\${sms.sender}")
+  private val sender: String,
+  @Value("\${sms.receiver}")
+  private val receiver: String,
+) : SmsService {
+  override fun send(message: String?) {
+    val solapiClient = SolapiClient.createInstance(smsKey, smsSecretKey)
+    val message = Message(from = sender, to = receiver, text = message)
+    solapiClient.send(message)
+
+  }
+}

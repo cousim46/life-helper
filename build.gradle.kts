f10fe910e0fcce56b9b1604c57db19ad9c40b052
cousim46 <wjdghldns24@naver.com>
2025-10-19 18:00:25 +0900

    config : 코틀린 버전 변경, coolSms sdk 추가, Spring ai 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-  kotlin("jvm") version "1.9.25"
-  kotlin("plugin.spring") version "1.9.25"
+  kotlin("jvm") version "2.2.0"
+  kotlin("plugin.spring") version "2.2.0"
   id("org.springframework.boot") version "3.5.4"
   id("io.spring.dependency-management") version "1.1.7"
-  kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "life"
@@ -17,23 +16,27 @@ java {
 
 repositories {
   mavenCentral()
+  maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  runtimeOnly("com.mysql:mysql-connector-j")
+  implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  //sms
+  implementation("com.solapi:sdk:1.0.3")
 }
 
 kotlin {
   compilerOptions {
-    freeCompilerArgs.addAll("-Xjsr305=strict")
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    freeCompilerArgs.add("-Xjsr305=strict")
   }
 }
 
@@ -45,4 +48,13 @@ allOpen {
 
 tasks.withType<Test> {
   useJUnitPlatform()
+}
+tasks.withType<JavaCompile> {
+  options.encoding = "UTF-8"
+}
+
+dependencyManagement {
+  imports {
+    mavenBom("org.springframework.ai:spring-ai-bom:1.0.0-M6")
+  }
 }
